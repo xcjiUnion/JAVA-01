485213f375dffb303882c351f7b7471f80ece0a4
JiXiaoChuang <jixch_101@163.com>
2021-01-22 23:55:43 +0800

    code

---
+++ Week_02/HttpClientTest.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class HttpClientTest {
+
+	public static void main(String[] args) throws IOException {
+		OkHttpClient client = new OkHttpClient();
+		String url = "http://localhost:8001";
+		
+		Request request = new Request.Builder().url(url).build();
+
+		Response response = client.newCall(request).execute();
+		System.out.println(response.body().string());
+		
+	}
+
+}
